23158fea693540010a8a2c795dda7f6a9719126a
surreal-ceres <the_ekj.2121@outlook.com>
2021-10-05 20:38:31 -0500

    Created main header

--- os/os.hpp
+++ os/os.hpp
@@ -1,4 +1,10 @@
 #ifndef _OS_OS_HPP
 #define _OS_OS_HPP
 
+#include "os/system.hpp"
+#include "os/window_base.hpp"
+#include "os/window.hpp"
+#include "os/surface.hpp"
+#include "os/event.hpp"
+
 #endif
