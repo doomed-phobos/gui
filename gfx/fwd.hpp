5d859feccfbefb0092388e0acbe8cf81561d8648
surreal-ceres <the_ekj.2121@outlook.com>
2021-10-05 20:39:05 -0500

    Changed fwd

--- gfx/fwd.hpp
+++ gfx/fwd.hpp
@@ -1,16 +1,8 @@
 #ifndef _GFX_FWD_HPP
 #define _GFX_FWD_HPP
 
-class SkCanvas;
-class SkBitmap;
-class SkPath;
-class SkPaint;
-class SkFont;
-
 namespace gfx
 {
-   class Paint;
-
    template<typename T> class PointT;
    template<typename T> class SizeT;
    template<typename T> class RectT;
